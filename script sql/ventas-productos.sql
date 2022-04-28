da41501c2f3b2596067e38a3a9c6ade06271b1c0
Aldair Torrez <torrezaldair777@gmail.com>
2022-04-28 19:16:31 -0400

    sql desechos y facturas

--- script sql/ventas-productos.sql
+++ script sql/ventas-productos.sql
@@ -1,10 +1,16 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     27/4/2022 20:03:25                           */
+/* Created on:     28/4/2022 19:14:30                           */
 /*==============================================================*/
 
 
 drop table if exists CLIENTE;
+
+drop table if exists CONTIENE;
+
+drop table if exists DESECHADO;
+
+drop table if exists FACTURA_COMPRA;
 
 drop table if exists LOTE;
 
@@ -32,6 +38,40 @@ create table CLIENTE
 );
 
 /*==============================================================*/
+/* Table: CONTIENE                                              */
+/*==============================================================*/
+create table CONTIENE
+(
+   NOMBREMED            char(256) not null,
+   ID_VENTA             char(256) not null,
+   primary key (NOMBREMED, ID_VENTA)
+);
+
+/*==============================================================*/
+/* Table: DESECHADO                                             */
+/*==============================================================*/
+create table DESECHADO
+(
+   MARCA                char(256),
+   FECHA_DESECHO        date,
+   CANTIDAD_DESECHADA   int,
+   TOTAL_PERDIDO        float(8,2),
+   MOTIVO_DESECHO       char(256)
+);
+
+/*==============================================================*/
+/* Table: FACTURA_COMPRA                                        */
+/*==============================================================*/
+create table FACTURA_COMPRA
+(
+   ID_FACTURA           char(256) not null,
+   ID_PEDIDO            char(256),
+   EMPRESA              char(256),
+   FECHA_FACTURA        date,
+   primary key (ID_FACTURA)
+);
+
+/*==============================================================*/
 /* Table: LOTE                                                  */
 /*==============================================================*/
 create table LOTE
@@ -48,7 +88,6 @@ create table LOTE
 create table MEDICAMENTO
 (
    NOMBREMED            char(256) not null,
-   ID_VENTA             char(256),
    CONTENIDO            char(256) not null,
    primary key (NOMBREMED)
 );
@@ -59,7 +98,7 @@ create table MEDICAMENTO
 create table PEDIDO
 (
    ID_PEDIDO            char(256) not null,
-   EMPRESA              char(256) not null,
+   ID_FACTURA           char(256),
    FECHA_PEDIDO         date,
    primary key (ID_PEDIDO)
 );
@@ -112,17 +151,29 @@ create table VENTA
    primary key (ID_VENTA)
 );
 
+alter table CONTIENE add constraint FK_CONTIENE foreign key (NOMBREMED)
+      references MEDICAMENTO (NOMBREMED) on delete restrict on update restrict;
+
+alter table CONTIENE add constraint FK_CONTIENE2 foreign key (ID_VENTA)
+      references VENTA (ID_VENTA) on delete restrict on update restrict;
+
+alter table DESECHADO add constraint FK_RELATIONSHIP_10 foreign key (MARCA)
+      references VARIEDAD (MARCA) on delete restrict on update restrict;
+
+alter table FACTURA_COMPRA add constraint FK_RELATIONSHIP_8 foreign key (ID_PEDIDO)
+      references PEDIDO (ID_PEDIDO) on delete restrict on update restrict;
+
+alter table FACTURA_COMPRA add constraint FK_RELATIONSHIP_9 foreign key (EMPRESA)
+      references PROVEEDOR (EMPRESA) on delete restrict on update restrict;
+
 alter table LOTE add constraint FK_RELATIONSHIP_6 foreign key (ID_PEDIDO)
       references PEDIDO (ID_PEDIDO) on delete restrict on update restrict;
 
 alter table LOTE add constraint FK_VIENE_CON foreign key (MARCA)
       references VARIEDAD (MARCA) on delete restrict on update restrict;
 
-alter table MEDICAMENTO add constraint FK_CONTIENE foreign key (ID_VENTA)
-      references VENTA (ID_VENTA) on delete restrict on update restrict;
-
-alter table PEDIDO add constraint FK_RELATIONSHIP_7 foreign key (EMPRESA)
-      references PROVEEDOR (EMPRESA) on delete restrict on update restrict;
+alter table PEDIDO add constraint FK_RELATIONSHIP_11 foreign key (ID_FACTURA)
+      references FACTURA_COMPRA (ID_FACTURA) on delete restrict on update restrict;
 
 alter table VARIEDAD add constraint FK_TIENE foreign key (NOMBREMED)
       references MEDICAMENTO (NOMBREMED) on delete restrict on update restrict;
